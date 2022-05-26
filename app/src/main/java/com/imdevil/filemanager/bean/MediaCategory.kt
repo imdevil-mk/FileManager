6524951a157c895d76f0ac5816c5bad09ecac89e
imdevil <516959708@qq.com>
2022-05-26 10:22:09 +0800

    add icon

--- app/src/main/java/com/imdevil/filemanager/bean/MediaCategory.kt
+++ app/src/main/java/com/imdevil/filemanager/bean/MediaCategory.kt
@@ -3,7 +3,8 @@ package com.imdevil.filemanager.bean
 import com.imdevil.filemanager.R
 
 enum class MediaCategory(val title: Int, val icon: Int) {
-    IMAGE(R.string.media_image, R.drawable.ic_launcher_foreground),
-    VIDEO(R.string.media_video, R.drawable.ic_launcher_foreground),
-    AUDIO(R.string.media_audio, R.drawable.ic_launcher_foreground)
+    NULL(-1, -1),
+    IMAGE(R.string.media_image, R.drawable.ic_image),
+    VIDEO(R.string.media_video, R.drawable.ic_video),
+    AUDIO(R.string.media_audio, R.drawable.ic_audio)
 }
